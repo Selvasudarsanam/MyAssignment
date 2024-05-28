62b577cfc1e85fe1f7b99b7a31106f461405f213
Selvasudarsanam <selvasudarsanam@gmail.com>
2024-05-29 00:04:47 +0530

    Add files via upload

---
+++ day2/fibnociSeries.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class fibnociSeries {
+
+	public static void main(String[] args) {
+		int i,a=1,b=2,c;
+		System.out.println(a);
+		System.out.println(b);
+		for (i=1; i<=10;i++) {
+			c=a+b;
+		System.out.println(c);
+		
+		a=b;b=c;
+				
+	}
+
+}
+}
